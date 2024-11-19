eac0c668098e8d8e786d22280ababbc88cfd857e
cuan1210 <quannhph50104@gmail.com>
2024-11-19 18:29:24 +0700

    lên code

--- view/header.php
+++ view/header.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DỰ ÁN MẪU</title>
+    <title>DỰ ÁN 1</title>
     <link rel="stylesheet" href="view/css/style.css">
 </head>
 <body>
     <div class="boxcenter">
         <div class="row mb header">
-            <h1>GARA Ô TÔ</h1>
+            <h1>TrailForge</h1>
         </div>
         
         <div class="row mb menu">
