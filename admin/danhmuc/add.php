f7b7d0cd6ba1b4b96d10b3787a50213c92592e89
anhhtph50205 <anhhtph50205@gmail.com>
2024-11-19 16:23:34 +0700

    Ta đẩy code

---
+++ admin/danhmuc/add.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="row formtitle">
+        <h1>THÊM MỚI HÀNG HÓA</h1>
+    </div>
+
+    <div class="row formcontent">
+        <form action="index.php?act=adddm" method="post">
+            <div class="row mb10">
+                <label for="maloai">Mã loại</label> 
+                <input type="text" name="maloai" disabled>
+            </div>
+
+            <div class="row mb10">
+                <label for="tenloai">Tên loại</label>
+                <input type="text" name="tenloai">
+            </div>
+
+            <div class="row mb10">
+                <input type="submit" name="themmoi" value="Thêm mới">
+                <input type="reset" value="Nhập lại">
+                <a href="index.php?act=listdm"><input type="button" value="Danh sách danh mục"></a>
+            </div>
+
+            <?php
+                if (isset($thongBao) && ($thongBao != "")) echo $thongBao;
+            ?>
+        </form>
+    </div>
+    
+</div>
